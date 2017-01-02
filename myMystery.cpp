4b0371db16a18fda4683bec98c735b8896329482
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-01-01 20:07:35 -0500

    Update myMystery.cpp

--- myMystery.cpp
+++ myMystery.cpp
@@ -22,29 +22,29 @@ int main(void)
 
 {
 
-int a = 3;
+	int a = 3;
 
-double b = 2.5;
+	double b = 2.5;
 
-int c = 1;
+	int c = 1;
 
-cout << " answer 1 is " << mystery(a, 2, c) << endl;
+	cout << " answer 1 is " << mystery(a, 2, c) << endl;
 
-cout << " answer 2 is " << mystery(a, 1, c) << endl;
+	cout << " answer 2 is " << mystery(a, 1, c) << endl;
 
-a = 5;
+	a = 5;
 
-cout << " answer 3 is " << mystery(a, b, c) << endl;
+	cout << " answer 3 is " << mystery(a, b, c) << endl;
 
-a = 2;
+	a = 2;
 
-b = 2.5;
+	b = 2.5;
 
-cout << " answer 4 is " << mystery(a, b, b) << endl;
+	cout << " answer 4 is " << mystery(a, b, b) << endl;
 
-cout << " answer 5 is " << mystery(a,mystery(a, b, b),c)<< endl;
+	cout << " answer 5 is " << mystery(a,mystery(a, b, b),c)<< endl;
 
-return 0;
+	return 0;
 
 }
 
